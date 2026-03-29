27798efb511b6042b11f657ececf1f1240f934b1
Pavan Varma <pavanvarma058@gmail.com>
2026-03-29 23:42:56 +0100

    learned about modules in nodejs

---
+++ 02-Modules/index.js
@@ -0,0 +1,6 @@
+import { add, subtract, count } from "./utils.js";
+
+count(5);
+
+console.log(add(2, 5));
+console.log(subtract(5, 1));
---
+++ 02-Modules/utils.js
@@ -0,0 +1,13 @@
+const count = (count) => {
+  console.log(count);
+};
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+export { count, add, subtract };
